d96cef48af8824d11d2a09228934d2a80cd4b627
Yasin Özbek <74540905+yasinzbk@users.noreply.github.com>
2024-01-27 21:00:07 +0300

    Efekt Duzeltme

--- Assets/Efects/EnemyController.cs
+++ Assets/Efects/EnemyController.cs
@@ -8,4 +8,10 @@ public class EnemyController: MonoBehaviour
     {
         throw new NotImplementedException();
     }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        Debug.Log(other.gameObject.name);
+
+    }
 }
