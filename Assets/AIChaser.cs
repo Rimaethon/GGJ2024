f1b88c71be62387894e8e096e1f0ffa00f149eab
Yoldaş Ulaş <yutalas@gmail.com>
2024-01-28 14:54:21 +0300

    AI Chaser Script Added

---
+++ Assets/AIChaser.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIChaser : MonoBehaviour
+{
+    public Transform target;
+    private NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if (target != null)
+        {
+            agent.SetDestination(target.position);
+        }
+    }
+}
