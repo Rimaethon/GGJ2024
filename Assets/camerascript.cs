9ddb4bb56dc624afecb9393f0c19ee1e1f6e1041
Yoldaş Ulaş <yutalas@gmail.com>
2024-01-28 03:51:56 +0300

    Camera Controller Added

---
+++ Assets/camerascript.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camerascript : MonoBehaviour
+{
+    public float rotationSpeed = 1;
+    public Transform root;
+
+    float mouseX, mouseY;
+
+    public float stomachOffset;
+
+    public ConfigurableJoint hipJoint, stomachJoint;
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        CamControl();
+    }
+
+    public void CamControl(){
+        mouseX += Input.GetAxis("Mouse X") * rotationSpeed;
+        mouseY -= Input.GetAxis("Mouse Y") * rotationSpeed;
+        mouseY = Mathf.Clamp(mouseY, -35, 60);
+
+        Quaternion rootRotation = Quaternion.Euler(mouseY, mouseX, 0);
+
+        root.rotation = rootRotation;
+
+    
+    }
+}
